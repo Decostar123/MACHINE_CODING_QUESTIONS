245f9c1cb1a871f598db94b4bb4ccb1bf1ee138b
Decostar <73633175+Decostar123@users.noreply.github.com>
2024-09-14 14:52:12 +0530

    modify notes of Projects

--- Pagination/pagination/src/notes.js
+++ Pagination/pagination/src/notes.js
@@ -5,9 +5,12 @@ let notes = `
     3) slice for client side pagination 
     4) useEfet for client side 
     5) Explanation of object-fit values:
+
+    https://www.w3schools.com/css/css3_object-fit.asp 
+    
     object-fit: cover;: The image is scaled to cover the entire container while maintaining its aspect ratio. The image might be clipped to fit.
     object-fit: contain;: The image is scaled to maintain its aspect ratio while fitting within the container. There might be empty space around the image.
-    object-fit: fill;: The image is stretched to fit the container's dimensions, which might distort the image.
+    object-fit: fill;: This is defualt ;The image is stretched to fit the container's dimensions, which might distort the image.
     object-fit: none;: The image is not resized and retains its original size.
     object-fit: scale-down;: The image is scaled down to the smallest possible size that fits within the container while maintaining its aspect ratio.    
 
